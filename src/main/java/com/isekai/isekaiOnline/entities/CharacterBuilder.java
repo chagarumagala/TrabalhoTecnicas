fb9f37f69088ead0b76c071c0f01c55e02d6e13a
Pedro Carlucci <pkcarlucci@gmail.com>
2020-06-23 23:36:23 -0300

    Array dos status dos perrsonagens e monstros

--- src/main/java/com/isekai/isekaiOnline/entities/CharacterBuilder.java
+++ src/main/java/com/isekai/isekaiOnline/entities/CharacterBuilder.java
@@ -1,35 +1,40 @@
 package com.isekai.isekaiOnline.entities;
 
 public class CharacterBuilder implements Builder{
+    int[] stats;
+    
+    public CharacterBuilder(){
+        stats = new int[7];
+    }
 
     @Override
     public void addStr(int str) {
         // TODO Auto-generated method stub
-
+        stats[0] = str;
     }
 
     @Override
     public void addDex(int dex) {
         // TODO Auto-generated method stub
-
+        stats[1] = dex;
     }
 
     @Override
     public void addCon(int con) {
         // TODO Auto-generated method stub
-
+        stats[2] = con;
     }
 
     @Override
     public void addWis(int wis) {
         // TODO Auto-generated method stub
-
+        stats[3] = wis;
     }
 
     @Override
     public void addInt(int intel) {
         // TODO Auto-generated method stub
-
+        stats[4] = intel;
     }
 
    
