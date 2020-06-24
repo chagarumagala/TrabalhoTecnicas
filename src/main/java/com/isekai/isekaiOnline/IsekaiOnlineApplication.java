0cc82ef1e1e5b54ec5cbbda1e9515c5d5e9c0ba4
Hiram Artnak Martins <hiram.martins@edu.pucrs.br>
2020-06-23 21:33:48 -0300

    initial spring commit

---
+++ src/main/java/com/isekai/isekaiOnline/IsekaiOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.isekai.isekaiOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsekaiOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsekaiOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isekai/isekaiOnline/IsekaiOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isekai.isekaiOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsekaiOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
