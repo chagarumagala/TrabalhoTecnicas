d51e11443d633e8428f84faa4e6472e4a65d28ff
Hiram Artnak Martins <hiram.martins@edu.pucrs.br>
2020-06-23 22:05:55 -0300

    Item class done

--- src/main/java/com/isekai/isekaiOnline/entities/Item.java
+++ src/main/java/com/isekai/isekaiOnline/entities/Item.java
@@ -1,5 +1,30 @@
 package com.isekai.isekaiOnline.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Item {
-    
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY) long id;
+    private String name;
+    private String description;
+    private int[] statusChange = new int[7];
+
+    public Item(String name, String description, int[] statusChange){
+        this.name = name;
+        this.description = description;
+        this.statusChange = statusChange;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public int[] getStatusChange(){
+        return this.statusChange;
+    }
 }
