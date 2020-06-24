977d90cff5869ba8109b5350d14d2d14ab78d6ae
Pedro Carlucci <pkcarlucci@gmail.com>
2020-06-24 01:36:24 -0300

    partyLeader

--- src/main/java/com/isekai/isekaiOnline/entities/PartyLeader.java
+++ src/main/java/com/isekai/isekaiOnline/entities/PartyLeader.java
@@ -4,5 +4,19 @@ import javax.persistence.Entity;
 
 @Entity
 public class PartyLeader{
-    
+    Character leader;
+    Party party;
+
+    public PartyLeader(Character leader, Party party){
+        this.leader = leader;
+        this.party = party;
+    }
+
+    public Party getParty(){
+        return party;
+    }
+
+    public Character getLeader(){
+        return leader;
+    }
 }
